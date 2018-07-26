3319fc09da1a0e36f1abd6269b63995aa4c7348d
Ricardo Robaina <ricardorobaina11@gmail.com>
2018-07-26 18:43:15 -0300

    Issue Latency: solved

--- app/src/main/java/com/example/ricardo/miniandoidmpc/MainActivity.java
+++ app/src/main/java/com/example/ricardo/miniandoidmpc/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.ricardo.miniandoidmpc;
 
 import android.media.MediaPlayer;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     // os botoes
     private ImageView btn1, btn2, btn3, btn4, btn5, btn6;
@@ -27,17 +27,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn5 = (ImageView) findViewById(R.id.btn5ID);
         btn6 = (ImageView) findViewById(R.id.btn6ID);
 
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
-        btn6.setOnClickListener(this);
+        btn1.setOnTouchListener(this);
+        btn2.setOnTouchListener(this);
+        btn3.setOnTouchListener(this);
+        btn4.setOnTouchListener(this);
+        btn5.setOnTouchListener(this);
+        btn6.setOnTouchListener(this);
 
     }
 
+
+    /*
     @Override
-    public void onClick(View view) {
+    public void onTouch()(View view) {
         switch (view.getId()) {
             case R.id.btn1ID:
                 mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bumbo);
@@ -66,6 +68,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+    */
+
 
 
 
@@ -94,4 +98,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (view.getId()) {
+            case R.id.btn1ID:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bumbo);
+                tocarSom();
+                break;
+            case R.id.btn2ID:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.boom);
+                tocarSom();
+                break;
+            case R.id.btn3ID:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.caixa);
+                tocarSom();
+                break;
+            case R.id.btn4ID:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bop);
+                tocarSom();
+                break;
+            case R.id.btn5ID:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.chipofechado);
+                tocarSom();
+                break;
+            case R.id.btn6ID:
+                mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.chipoaberto);
+                tocarSom();
+                break;
+
+        }
+        return false;
+    }
 }
